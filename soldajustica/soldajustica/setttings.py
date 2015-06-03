d14d9502dc278f08eb92d038f6aa6ae1d8441664
Patrick Porto <patrick.porto@concretesolutions.com.br>
2015-06-03 20:41:22 -0300

    Configuração do banco de produção

--- soldajustica/soldajustica/setttings.py
+++ soldajustica/soldajustica/setttings.py
@@ -3,14 +3,10 @@ Django settings for soldajustica project.
 """
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 from os import (path, environ,)
-import dj_database_url
 PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = (
     'localhost',
@@ -22,21 +18,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'local': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
-    'production': dj_database_url.config()
-}
-if DATABASES['production']:
-    DATABASES['default'] = DATABASES['production']
-else:
-    DATABASES['default'] = DATABASES['local']
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 LOGIN_URL = '/login'
 
@@ -53,6 +36,17 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'pt-br'
 
+DATABASES = {
+    'local': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
@@ -68,18 +62,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = environ.get('DJANGO_SETTINGS_MEDIA_ROOT', path.join(PROJECT_ROOT, 'media').replace('\\', '/'))
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = environ.get('DJANGO_SETTINGS_STATIC_ROOT', path.join(PROJECT_ROOT, 'static').replace('\\', '/'))
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
