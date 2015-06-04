8d9564a7b53943b054bfcd663ef9cb42d2c15c5b
Patrick Porto <patrick.porto@concretesolutions.com.br>
2015-06-03 22:58:23 -0300

    Excluindo autenticação do site

---
+++ soldajustica/app/urls.py
@@ -0,0 +1,10 @@
+"""
+Definition of urls for soldajustica.
+"""
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^contact$', 'app.views.contact', name='contact'),
+    url(r'^about', 'app.views.about', name='about'),
+)
--- soldajustica/soldajustica/urls.py
+++ soldajustica/soldajustica/urls.py
@@ -11,27 +11,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'app.views.home', name='home'),
-    url(r'^contact$', 'app.views.contact', name='contact'),
-    url(r'^about', 'app.views.about', name='about'),
-    url(r'^login/$',
-        'django.contrib.auth.views.login',
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title':'Log in',
-                'year':datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        'django.contrib.auth.views.logout',
-        {
-            'next_page': '/',
-        },
-        name='logout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('app.urls')),
 )
